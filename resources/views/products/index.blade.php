bbdfae1fb1cca634a6d3bf389ff82627e4ae9fd6
Md. Sajid Salim <sajidsalim46@gmail.com>
2021-11-10 01:42:26 +0600

    view product list

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -51,11 +51,11 @@
                     </thead>
 
                     <tbody>
-
+                    @foreach( $products as $product )
                     <tr>
-                        <td>1</td>
-                        <td>T-Shirt <br> Created at : 25-Aug-2020</td>
-                        <td>Quality product in low cost</td>
+                        <td>{{$product->id}}</td>
+                        <td>{{$product->title}} <br> Created at : {{ date("d-M-Y",strtotime($product->created_at))}}</td>
+                        <td>{{$product->description}}</td>
                         <td>
                             <dl class="row mb-0" style="height: 80px; overflow: hidden" id="variant">
 
@@ -77,6 +77,7 @@
                             </div>
                         </td>
                     </tr>
+                    @endforeach
 
                     </tbody>
 
